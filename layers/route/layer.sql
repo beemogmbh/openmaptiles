afcc8657a6360f243266d0fd26fded5aa921b29b
Arne Kaiser <onkelarne@gmail.com>
2020-01-29 18:13:47 +0100

    Use generalized tables for route layer

--- layers/route/layer.sql
+++ layers/route/layer.sql
@@ -1,6 +1,37 @@
 CREATE OR REPLACE FUNCTION layer_route(bbox geometry, zoom_level integer)
 RETURNS TABLE(osm_id bigint, geometry geometry, class text, network text, ref text, name text) AS $$
     SELECT member AS osm_id, geometry, type AS class, network, ref, name
-    FROM osm_route_relation_members
-    WHERE zoom_level >= 10 AND zoom_level <= 14 AND geometry && bbox;
+    FROM (
+
+        SELECT member, geometry, type, network, ref, name
+        FROM osm_route_relation_members_gen5
+        WHERE zoom_level = 10
+        
+        UNION ALL
+
+        SELECT member, geometry, type, network, ref, name
+        FROM osm_route_relation_members_gen4
+        WHERE zoom_level = 11
+        
+        UNION ALL
+
+        SELECT member, geometry, type, network, ref, name
+        FROM osm_route_relation_members_gen3
+        WHERE zoom_level = 12
+        
+        UNION ALL
+
+        SELECT member, geometry, type, network, ref, name
+        FROM osm_route_relation_members_gen2
+        WHERE zoom_level = 13
+        
+        UNION ALL
+
+        SELECT member, geometry, type, network, ref, name
+        FROM osm_route_relation_members_gen1
+        WHERE zoom_level >= 14
+
+    ) AS zoom_levels
+    WHERE geometry && bbox;
+
 $$ LANGUAGE SQL IMMUTABLE;
